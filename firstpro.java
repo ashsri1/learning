32356c183c8d78486fc6e1f27d6752232141a4fd
ashsri1 <65027963+ashsri1@users.noreply.github.com>
2023-05-06 20:38:19 +0530

    My first file
    Signed-off-by: ashsri1 <65027963+ashsri1@users.noreply.github.com>

--- firstpro.java
+++ firstpro.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class firstpro {
-}
+public class firstpro {
+
+        public static void main(String[] args) {
+            int[] arr = {1, 2, 3, 4, 5};
+            int target = 2;
+            boolean found = false;
+            for (int i = 0; i < arr.length; i++) {
+                System.out.println(i);
+                if (arr[i] == target) {
+                    System.out.println("found");
+                    found = true;
+                    break;
+                }
+
+            }
+            if (!found) {
+                System.out.println("not found");
+            }
+        }
+    }
+
+
+
